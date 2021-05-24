ebd7f549dae818188138e5e685f2cd8656134ba9
Pat McLean <patrick.mclean@sap.com>
2021-05-24 12:56:19 +0000

    wrote admin pages tests for accounts

--- api/core/tests/test_admin.py
+++ api/core/tests/test_admin.py
@@ -2,6 +2,8 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from core.models import Account
+
 
 class AdminSiteTests(TestCase):
 
@@ -17,6 +19,7 @@ class AdminSiteTests(TestCase):
             password='password123',
             name='Test user full name'
         )
+        self.account = Account.objects.create(user=self.user, name='SAP')
 
     def test_users_listed(self):
         """Test that users are listed on user page"""
@@ -37,6 +40,28 @@ class AdminSiteTests(TestCase):
     def test_create_user_page(self):
         """Test that the create user page works"""
         url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+        # Assertions ( checking for http 200 )
+        self.assertEqual(res.status_code, 200)
+
+    def test_accounts_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse('admin:core_account_changelist')
+        res = self.client.get(url)
+        # Assertions
+        self.assertContains(res, self.account.name)
+
+    def test_account_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_account_change', args=[self.account.id])
+        # above line generates url like   /admin/core/user/17 (id)
+        res = self.client.get(url)
+        # Assertions ( checking for http 200 )
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_account_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_account_add')
         res = self.client.get(url)
         # Assertions ( checking for http 200 )
         self.assertEqual(res.status_code, 200)
