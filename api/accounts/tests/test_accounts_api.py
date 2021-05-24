3b7704e96eba7a0e76e4dbb302900fe9f652e2bb
Pat McLean <patrick.mclean@sap.com>
2021-05-24 13:02:05 +0000

    Added acccounts app, and tests for account objects

---
+++ api/accounts/tests/test_accounts_api.py
@@ -0,0 +1,87 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Account
+
+from accounts.serializers import AccountSerializer
+
+
+ACCOUNT_URL = reverse('accounts:account-list')
+
+
+class PublicAccountsApiTests(TestCase):
+    """Test the publicly available accounts API"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_login_required(self):
+        """Test that login required for retrieving accounts"""
+        res = self.client.get(ACCOUNT_URL)
+        # Assertion
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PrivateAccountsApiTests(TestCase):
+    """Test the authorized user accounts API"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            'test@psmware.io',
+            'password'
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_accounts(self):
+        """Test retrieving accounts"""
+        Account.objects.create(
+            user=self.user, name='SAP')
+        Account.objects.create(user=self.user, name='BSNIC')
+
+        res = self.client.get(ACCOUNT_URL)
+
+        accounts = Account.objects.all().order_by('name')
+        serializer = AccountSerializer(accounts, many=True)
+        # Assertions
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
+
+    def test_accounts_limited_to_user(self):
+        """Test that accounts returned are for authenticated user"""
+        user2 = get_user_model().objects.create_user(
+            'other@psmware.io',
+            'testpass'
+        )
+        Account.objects.create(user=user2, name='BSNIC')
+        account = Account.objects.create(
+            user=self.user, name='SAP')
+
+        res = self.client.get(ACCOUNT_URL)
+        # Assertions
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], account.name)
+
+    def test_create_account_successful(self):
+        """Test creating a new account"""
+        payload = {'name': 'BSNIC'}
+        self.client.post(ACCOUNT_URL, payload)
+
+        exists = Account.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        # Assertion
+        self.assertTrue(exists)
+
+    def test_create_account_invalid(self):
+        """Test creating a new account with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(ACCOUNT_URL, payload)
+        # Assertion
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
