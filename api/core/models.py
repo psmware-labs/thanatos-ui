28ef518ece25ebd2a8a8f09fdb1f57a92c8256a8
Pat McLean <patrick.mclean@sap.com>
2021-05-24 13:57:06 +0000

    Added code to pass tests for metrics models

--- api/core/models.py
+++ api/core/models.py
@@ -50,3 +50,39 @@ class Account(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class PrimaryMetric(models.Model):
+    """Primary Metrics to be used for a group of questions"""
+    metric = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.metric
+
+
+class SecondaryMetric(models.Model):
+    """Secondary Metrics to be used for a group of questions"""
+    metric = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.metric
+
+
+class TertiaryMetric(models.Model):
+    """Tertiary Metrics to be used for a group of questions"""
+    metric = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.metric
